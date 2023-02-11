f83c1d449338440f629be715df8669c87afc0da8
Şenol GÖKNER <senol.gokner@gmail.com>
2023-02-11 21:47:04 +0300

    update

---
+++ body.php
@@ -0,0 +1,7 @@
+<div class="container border shadow-sm mt-2 mb-2">
+    <div class="row">
+    <p>
+    Deneme
+</p>
+    </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="container  shadow-sm border  bg-light p-2">
+    <div class="row">
+        <div class="col-4">
+            <p class="small">
+            Şenol GÖKNER <br> </p>
+            <p class="small">Mimar İTÜ 2006</p>
+           
+                </div>
+        <div class="col-4"></div>
+        <div class="col-4"></div>
+    </div>
+</div>
